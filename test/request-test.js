0416db4de2112d03e04b750ce627101c7fd054cd
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2017-06-29 23:23:31 +0200

    Remove inner describe block

--- test/request-test.js
+++ test/request-test.js
@@ -1,34 +1,31 @@
 const Request = require('../lib/request');
 
-describe('Request unit tests', function() {
-    describe("matching", function() {
-
-        it('should work with defined headers in the incoming request', function() {
-            const request = new Request({}, {
-                method: 'GET',
-                url: '/lowercasetest',
-                headers: { 'foo-type': 'artischocke' }
-            });
-
-            expect(request).toEqual(expect.objectContaining({
-                method: 'GET',
-                url: '/lowercasetest',
-                headers: { 'foo-type': 'artischocke' }
-            }));
+describe('request', function() {
+    it('should work with defined headers in the incoming request', function() {
+        const request = new Request({}, {
+            method: 'GET',
+            url: '/lowercasetest',
+            headers: { 'foo-type': 'artischocke' }
         });
 
-        it('should work with defined headers in the incoming request', function() {
-            const request = new Request({}, {
-                method: 'GET',
-                url: '/lowercasetest',
-                headers: { 'foo-type': 'artischocke' }
-            });
+        expect(request).toEqual(expect.objectContaining({
+            method: 'GET',
+            url: '/lowercasetest',
+            headers: { 'foo-type': 'artischocke' }
+        }));
+    });
 
-            expect(request).toEqual(expect.objectContaining({
-                method: 'GET',
-                url: '/lowercasetest',
-                headers: {'foo-type': 'artischocke'}
-            }));
+    it('should work with defined headers in the incoming request', function() {
+        const request = new Request({}, {
+            method: 'GET',
+            url: '/lowercasetest',
+            headers: { 'foo-type': 'artischocke' }
         });
+
+        expect(request).toEqual(expect.objectContaining({
+            method: 'GET',
+            url: '/lowercasetest',
+            headers: {'foo-type': 'artischocke'}
+        }));
     });
 });
